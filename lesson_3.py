941cb528bfb563399f9733c9760a5d0e9058ae5d
abdurazzak2002 <toloev.abi97@gmail.com>
2021-09-18 13:44:11 +0600

    1

---
+++ lesson_3.py
@@ -0,0 +1,25 @@
+# names = ['Abi', 'Dastan', 'Mirlan', 'Aza', 'Beka']
+# print(names[::2])
+
+
+
+# age = int(input('Введите возраст'))
+# if age >= 18:
+#     print('Прошу вас! Вы можете учавствовать в голосовании!')
+# else:
+#     print('К сожалению вы не сможете голосовать')
+
+
+score = int(input('Введите вашу оценку: '))
+
+if score > 90:
+    print('Отлично ваша оценка A!')
+elif score > 80:
+    print('Ваша оценка B')
+elif score > 70:
+    print('Ваша оценка C')
+elif score > 60:
+    print('Ваша оценка D')
+else :
+    print('Вы не сдали экзамен(')
+
